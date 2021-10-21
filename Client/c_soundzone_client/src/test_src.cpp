70580ee831e296063c097088e5733c91af61e239
Niels Dyrberg <ncpd@live.dk>
2021-10-21 15:29:47 +0200

    c client project base, added

---
+++ Client/c_soundzone_client/src/test_src.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ncpd on 21-10-2021.
+//
+
+#include "test_src.h"
+#include <iostream>
+
+int testSrcHello(){
+    std::cout << "Hello, World, from src!" << std::endl;
+    return 0;
+}
