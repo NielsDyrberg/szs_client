a264332ce7575b52154f2d48673a5a2feb5f4e7c
Niels Dyrberg <ncpd@live.dk>
2021-10-27 19:40:28 +0200

    commit for backup

--- Client/c_soundzone_client/main.cpp
+++ Client/c_soundzone_client/main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include "bcm2835.h"
-#include "alsadriver.h"
-//#include "alsadriver.h"
-//#include "asoundlib.h"
-
-
-
-alsadriver dacdriver;
 
 int main() {
-   // dacdriver.startstreaming(44100, 2, "SND_PCM_FORMAT_S16_LE");
-    dacdriver.playmusic();
-    dacdriver.checksetup();
+    std::cout << "Hello world, from main!" << std::endl;
     return 0;
 }
