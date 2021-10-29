280800ac3f5d27ef3fa0ea718ebd0e00e501e1de
Niels Dyrberg <ncpd@live.dk>
2021-10-29 15:52:34 +0200

    Rearanging files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "szs_soundzone_protocol.h"
 
 int main() {
     std::cout << "Hello world, from main!" << std::endl;
+    szp = sound_zone_protocol();
     return 0;
 }
