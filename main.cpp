1099c50f27c1fc5905a9915455901dea491f1515
Niels Dyrberg <ncpd@live.dk>
2021-10-29 16:17:48 +0200

    Sub module szp is now included

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include "szs_soundzone_protocol.h"
+#include "sound_zone_protocol.h"
 
 int main() {
     std::cout << "Hello world, from main!" << std::endl;
-    szp = sound_zone_protocol();
     return 0;
 }
