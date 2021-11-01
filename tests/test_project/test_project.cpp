6e15fb9cdc2c094b11d62204d88fecae7ae84886
Niels Dyrberg <ncpd@live.dk>
2021-11-01 09:34:03 +0100

    Added test

---
+++ tests/test_project/test_project.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ncpd on 21-10-2021.
+//
+#include <iostream>
+
+
+int test_projectLink(){
+    return 3;
+}
+
+int main(){
+    std::cout << "Hello world, from test_project" << std::endl;
+    test_projectLink();
+    return 0;
+}
